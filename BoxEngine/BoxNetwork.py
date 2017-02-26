Online hard example mining. Loss average fix. Allow k>n in gatherTopK.

BoxEngine/BoxNetwork.py
@@ -19,7 +19,7 @@ from BoxEngine.RPN import RPN
 import tensorflow as tf
 
 class BoxNetwork:
-	def __init__(self, nCategories, rpnLayer, rpnDownscale, rpnOffset, featureLayer=None, featureDownsample=None, featureOffset=None, weightDecay=1e-6):
+	def __init__(self, nCategories, rpnLayer, rpnDownscale, rpnOffset, featureLayer=None, featureDownsample=None, featureOffset=None, weightDecay=1e-6, hardMining=True):
 		if featureLayer is None:
 			featureLayer=rpnLayer
 
@@ -31,7 +31,7 @@ class BoxNetwork:
 
 		with tf.name_scope("BoxNetwork"):
 			self.rpn = RPN(rpnLayer, immediateSize=512, weightDecay=weightDecay, inputDownscale=rpnDownscale, offset=rpnOffset)
-			self.boxRefiner = BoxRefinementNetwork(featureLayer, nCategories, downsample=featureDownsample, offset=featureOffset)
+			self.boxRefiner = BoxRefinementNetwork(featureLayer, nCategories, downsample=featureDownsample, offset=featureOffset, hardMining=hardMining)
 
 			self.proposals, self.proposalScores = self.rpn.getPositiveOutputs(maxOutSize=300)
 

BoxEngine/BoxRefinementNetwork.py
@@ -26,12 +26,14 @@ import BoxEngine.Loss as Loss
 class BoxRefinementNetwork:
 	POOL_SIZE=3
 
-	def __init__(self, input, nCategories, downsample=16, offset=[32,32]):
+	def __init__(self, input, nCategories, downsample=16, offset=[32,32], hardMining=True):
 		self.downsample = downsample
 		self.offset = offset
 		self.nCategories = nCategories
 		self.classMaps = slim.conv2d(input, (self.POOL_SIZE**2)*(1+nCategories), 3, activation_fn=None, scope='classMaps')
 		self.regressionMap = slim.conv2d(input, (self.POOL_SIZE**2)*4, 3, activation_fn=None, scope='regressionMaps')
+
+		self.hardMining=hardMining
 
 		#Magic parameters.
 		self.posIouTheshold = 0.5
@@ -57,7 +59,7 @@ class BoxRefinementNetwork:
 			netScores = self.getBoxScores(boxes)
 			refOnehot = tf.one_hot(refs, self.nCategories+1, on_value=1.0 - self.nCategories*self.falseValue, off_value=self.falseValue)
 		
-			return tf.reduce_mean(tf.nn.softmax_cross_entropy_with_logits(netScores, refOnehot))
+			return tf.nn.softmax_cross_entropy_with_logits(netScores, refOnehot)
 
 	def refineBoxes(self, boxes):
 		with tf.name_scope("refineBoxes"):
@@ -78,11 +80,8 @@ class BoxRefinementNetwork:
 
 	def boxRefinementLoss(self, boxes, refBoxes):
 		with tf.name_scope("boxesRefinementLoss"):
-			def loss():
-				refinedBoxes, refSizes = self.refineBoxes(boxes)
-				return tf.reduce_mean(Loss.boxRegressionLoss(refinedBoxes, refBoxes, refSizes))
-
-			return tf.cond(tf.logical_and(tf.shape(boxes)[0]>0, tf.shape(refBoxes)[0]>0), lambda: loss(), lambda: tf.constant(0.0))
+			refinedBoxes, refSizes = self.refineBoxes(boxes)
+			return Loss.boxRegressionLoss(refinedBoxes, refBoxes, refSizes)
 
 	def loss(self, proposals, refBoxes, refClasses):
 		with tf.name_scope("BoxRefinementNetworkLoss"):
@@ -95,17 +94,19 @@ class BoxRefinementNetwork:
 					positiveClasses, positiveRefBoxes = MultiGather.gather([refClasses, refBoxes], positiveRefIndices)
 					positiveClasses = tf.cast(tf.cast(positiveClasses,tf.int8) + 1, tf.uint8)
 
-					selected = Utils.RandomSelect.randomSelectIndex(tf.shape(positiveBoxes)[0], nPositive)
-					positiveBoxes, positiveClasses, positiveRefBoxes = MultiGather.gather([positiveBoxes, positiveClasses, positiveRefBoxes], selected)
+					if not self.hardMining:
+						selected = Utils.RandomSelect.randomSelectIndex(tf.shape(positiveBoxes)[0], nPositive)
+						positiveBoxes, positiveClasses, positiveRefBoxes = MultiGather.gather([positiveBoxes, positiveClasses, positiveRefBoxes], selected)
 
 					return tf.tuple([self.classRefinementLoss(positiveBoxes, positiveClasses) + self.boxRefinementLoss(positiveBoxes, positiveRefBoxes), tf.shape(positiveBoxes)[0]])
 
 			def getNegLoss(negativeBoxes, nNegative):
 				with tf.name_scope("getNetLoss"):
-					negativeIndices = Utils.RandomSelect.randomSelectIndex(tf.shape(negativeBoxes)[0], nNegative)
-					negativeBoxes = tf.gather_nd(negativeBoxes, negativeIndices)
+					if not self.hardMining:
+						negativeIndices = Utils.RandomSelect.randomSelectIndex(tf.shape(negativeBoxes)[0], nNegative)
+						negativeBoxes = tf.gather_nd(negativeBoxes, negativeIndices)
 
-					return self.classRefinementLoss(negativeBoxes, tf.zeros(tf.shape(negativeIndices)[0:1], dtype=tf.uint8))
+					return self.classRefinementLoss(negativeBoxes, tf.zeros(tf.pack([tf.shape(negativeBoxes)[0],1]), dtype=tf.uint8))
 			
 			def getRefinementLoss():
 				with tf.name_scope("getRefinementLoss"):
@@ -130,11 +131,20 @@ class BoxRefinementNetwork:
 					nPositive = tf.shape(posBoxes)[0]
 					nNegative = tf.shape(negBoxes)[0]
 
+					if self.hardMining:
+						posLoss = tf.cond(nPositive > 0, lambda: getPosLoss(posBoxes, posRefIndices, 0)[0], lambda: tf.zeros((0,), tf.float32))
+						negLoss = tf.cond(nNegative > 0, lambda: getNegLoss(negBoxes, 0), lambda: tf.zeros((0,), tf.float32))
 
-					posLoss, posCount = tf.cond(nPositive > 0, lambda: getPosLoss(posBoxes, posRefIndices, self.nTrainPositives), lambda: tf.tuple([tf.constant(0.0), tf.constant(0,tf.int32)]))
-					negLoss = tf.cond(nNegative > 0, lambda: getNegLoss(negBoxes, self.nTrainBoxes-posCount), lambda: tf.constant(0.0))
+						allLoss = tf.concat(0,[posLoss, negLoss])
+						return tf.cond(tf.shape(allLoss)[0]>0, lambda: tf.reduce_mean(Utils.MultiGather.gatherTopK(allLoss, self.nTrainBoxes)), lambda: tf.constant(0.0))
+					else:
+						posLoss, posCount = tf.cond(nPositive > 0, lambda: getPosLoss(posBoxes, posRefIndices, self.nTrainPositives), lambda: tf.tuple([tf.constant(0.0), tf.constant(0,tf.int32)]))
+						negLoss = tf.cond(nNegative > 0, lambda: getNegLoss(negBoxes, self.nTrainBoxes-posCount), lambda: tf.constant(0.0))
+
+						nPositive = tf.cast(tf.shape(posLoss)[0], tf.float32)
+						nNegative = tf.cast(tf.shape(negLoss)[0], tf.float32)
 						
-					return posLoss + negLoss
+						return (tf.reduce_mean(posLoss)*nPositive + tf.reduce_mean(negLoss)*nNegative)/(nNegative+nPositive)
 	
 
 		return tf.cond(tf.logical_and(tf.shape(proposals)[0] > 0, tf.shape(refBoxes)[0] > 0), lambda: getRefinementLoss(), lambda:tf.constant(0.0))

BoxInceptionResnet.py
@@ -24,7 +24,7 @@ from BoxEngine.BoxNetwork import BoxNetwork
 
 
 class BoxInceptionResnet(BoxNetwork):
-	def __init__(self, inputs, nCategories, name="BoxNetwork", weightDecay=0.00004, reuse=False, isTraining=True, trainFrom=None):
+	def __init__(self, inputs, nCategories, name="BoxNetwork", weightDecay=0.00004, reuse=False, isTraining=True, trainFrom=None, hardMining=True):
 		self.boxThreshold = 0.5
 
 		if trainFrom == "0":
@@ -43,7 +43,7 @@ class BoxInceptionResnet(BoxNetwork):
 				scale_16 = self.googleNet.getOutput("Mixed_6a")
 				scale_32 = self.googleNet.getOutput("PrePool")
 
-				BoxNetwork.__init__(self, nCategories, scale_16, 16, [16,16], scale_32, 32, [32,32], weightDecay=weightDecay)
+				BoxNetwork.__init__(self, nCategories, scale_16, 16, [16,16], scale_32, 32, [32,32], weightDecay=weightDecay, hardMining=hardMining)
 
 				# scale_32 = tf.image.resize_bilinear(scale_32, tf.shape(scale_16)[1:3])
 

Utils/MultiGather.py
@@ -26,11 +26,12 @@ def gather(tensors, indices):
 def gatherTopK(t, k, others=[], sorted=False):
     res=[]
     with tf.name_scope("gather_top_k"):
-        values, indices = tf.nn.top_k(t, k=k, sorted=sorted)
+        isMoreThanK = tf.shape(t)[-1]>k
+        values, indices = tf.cond(isMoreThanK, lambda: tf.nn.top_k(t, k=k, sorted=sorted), lambda: tf.tuple([t, tf.zeros((0,1), tf.int32)]))
         indices = tf.reshape(indices, [-1,1])
         res.append(values)
 
         for o in others:
-            res.append(tf.gather_nd(o, indices))
+            res.append(tf.cond(isMoreThanK, lambda: tf.gather_nd(o, indices), lambda: o))
 
     return res

main.py
@@ -17,17 +17,10 @@
 # ==============================================================================
 
 
-import tensorflow as tf
-from tensorflow.python.ops import control_flow_ops
-from Dataset.CocoDataset import *
-from Utils.RunManager import *
-from Utils.CheckpointLoader import *
+
 from Utils.ArgSave import *
 import sys
-from BoxInceptionResnet import *
-from Dataset import Augment
-from Visualize import VisualizeOutput
-from Utils import Model
+import os
 
 parser = StorableArgparse(description='Kaggle fish trainer.')
 parser.add_argument('-learningRate', type=float, default=0.0001, help='Learning rate')
@@ -41,6 +34,8 @@ parser.add_argument('-optimizer', type=str, default='adam', help='sgd/adam/rmspr
 parser.add_argument('-resume', type=str, help='Resume from this file', save=False)
 parser.add_argument('-report', type=str, default="", help='Create report here', save=False)
 parser.add_argument('-trainFrom', type=str, default="-1", help='Train from this layer. Use 0 for all, -1 for just the added layers')
+parser.add_argument('-hardMining', type=int, default=1, help="Enable hard example mining.")
+parser.add_argument('-gpu', type=str, default="0", help='Train on this GPU(s)')
 
 opt=parser.parse_args()
 
@@ -49,9 +44,6 @@ if not os.path.isdir(opt.name):
 
 opt = parser.load(opt.name+"/args.json")
 parser.save(opt.name+"/args.json")
-
-globalStep = tf.Variable(0, name='globalStep', trainable=False)
-globalStepInc=tf.assign_add(globalStep,1)
 
 if not os.path.isdir(opt.name+"/log"):
 	os.makedirs(opt.name+"/log")
@@ -62,7 +54,22 @@ if not os.path.isdir(opt.name+"/save"):
 if not os.path.isdir(opt.name+"/preview"):
 	os.makedirs(opt.name+"/preview")
 
-#dataset = KaggleFishLoader(opt.dataset, randZoom=opt.randZoom==1)
+
+os.environ["CUDA_VISIBLE_DEVICES"] = opt.gpu
+
+import tensorflow as tf
+from tensorflow.python.ops import control_flow_ops
+from Dataset.CocoDataset import *
+from Utils.RunManager import *
+from Utils.CheckpointLoader import *
+from BoxInceptionResnet import *
+from Dataset import Augment
+from Visualize import VisualizeOutput
+from Utils import Model
+
+
+globalStep = tf.Variable(0, name='globalStep', trainable=False)
+globalStepInc=tf.assign_add(globalStep,1)
 
 Model.download()
 
@@ -76,7 +83,7 @@ images, boxes, classes = Augment.augment(*dataset.get())
 print("Number of categories: "+str(dataset.categoryCount()))
 
 
-net = BoxInceptionResnet(images, dataset.categoryCount(), name="boxnet", trainFrom=opt.trainFrom)
+net = BoxInceptionResnet(images, dataset.categoryCount(), name="boxnet", trainFrom=opt.trainFrom, hardMining=opt.hardMining==1)
 
 loss = net.getLoss(boxes, classes)
 slim.losses.add_loss(loss)